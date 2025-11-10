8ad388c522bd5aa885c8386b2157e012cb8202d5
dreyfle <norlander163@gmail.com>
2025-11-10 18:28:37 +0800

    changed type_choices in Transaction model

--- api/models/transactions.py
+++ api/models/transactions.py
@@ -3,11 +3,11 @@ from django.db import models
 class Transaction(models.Model):
   id = models.AutoField(primary_key=True)
   TYPE_CHOICES = [
-    ('inbound', 'Inbound'),
-    ('outbound', 'Outbound'),
+    ('restock', 'Restock'),
+    ('sale', 'Sale'),
     ('adjustment', 'Adjustment'),
   ]
-  type = models.CharField(max_length=15, choices=TYPE_CHOICES, default='outbound')
+  type = models.CharField(max_length=15, choices=TYPE_CHOICES, default='sale')
   quantity_change = models.IntegerField()
   unit_price_at_sale = models.DecimalField(max_digits=10, decimal_places=2, null=True)
   datetime_created = models.DateTimeField(auto_now_add=True)
